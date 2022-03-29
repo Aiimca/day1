209b53a5033e33037138006c5f41051ff7e09885
Aiimca <92739476+Aiimca@users.noreply.github.com>
2022-03-29 22:28:57 +0800

    Update 打印100-200素数.c

--- 打印100-200素数.c
+++ 打印100-200素数.c
@@ -1,20 +1,20 @@
-//#include<stdio.h>
-//int main() 
-//{
-//	for (int i = 100; i <= 200; i++) 
-//	{
-//		int a = 0;
-//		for(int j = 2; j < i; j++)
-//		{
-//			if (i % j == 0) 
-//			{
-//				a++;
-//			}
-//		}
-//		if (a == 0) 
-//		{
-//			printf("%d\n", i);
-//		}
-//	}
-//	return 0;
-//}
+#include<stdio.h>
+int main() 
+{
+	for (int i = 100; i <= 200; i++) 
+	{
+		int a = 0;
+		for(int j = 2; j < i; j++)
+		{
+			if (i % j == 0) 
+			{
+				a++;
+			}
+		}
+		if (a == 0) 
+		{
+			printf("%d\n", i);
+		}
+	}
+	return 0;
+}
